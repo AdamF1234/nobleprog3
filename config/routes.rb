899108829bfb897f3a4d734af8a5782e12214223
Adam Figura <adamos1998@gmail.com>
2022-04-11 16:30:21 +0000

    Post model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'privacy' , to: 'static_pages#privacy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+get 'aktualny_czas', to: 'static_pages#current_time'
   # Defines the root path route ("/")
   # root "articles#index"
 end
